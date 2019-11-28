797e54ada0a768d7f8230677e2acd9eabd299744
Dimitris Tsiamasiotis <dtsiamasiotis@gmail.com>
2019-11-28 18:37:42 +0200

    find the tickets played on a specific date

--- src/main/java/com/example/lotteryStatsService/LotteryStatsController.java
+++ src/main/java/com/example/lotteryStatsService/LotteryStatsController.java
@@ -1,11 +1,13 @@
 package com.example.lotteryStatsService;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(path="/stats",produces = "application/json")
@@ -18,8 +20,9 @@ public class LotteryStatsController {
     }
 
     @GetMapping("/test/{date}")
-    public String testGet(@PathVariable("date") Date d)
+    public String testGet(@PathVariable("date") @DateTimeFormat(pattern="ddMMyyyy") LocalDate minlimit)
     {
-        return String.valueOf(ticketRepository.findCountOfTicketsByDate(d));
+        LocalDate maxlimit = minlimit.plusDays(1);
+        return String.valueOf(ticketRepository.findCountOfTicketsByDate(minlimit,maxlimit));
     }
 }
--- src/main/java/com/example/lotteryStatsService/TicketRepository.java
+++ src/main/java/com/example/lotteryStatsService/TicketRepository.java
@@ -3,10 +3,11 @@ package com.example.lotteryStatsService;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public interface TicketRepository extends CrudRepository<Ticket,Long> {
 
-    @Query("select count(t) from Ticket t where date_played>=:dateplayed and date_played<=:dateplayed+1")
-    int findCountOfTicketsByDate(Date dateplayed);
+    @Query(value = "select count(*) from tickets where date_played>:minlimit and date_played<:maxlimit",nativeQuery = true)
+    int findCountOfTicketsByDate(LocalDate minlimit,LocalDate maxlimit);
 }
